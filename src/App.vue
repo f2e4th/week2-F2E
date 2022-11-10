c26942c5029639f244a99eb60bbc6bb51c1baf0b
Evalin0316 <59949752+Evalin0316@users.noreply.github.com>
2022-11-10 09:20:28 +0800

    feat:add page folder

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <template>
   <div>
-    hello
+    <home-page/>
   </div>
 </template>
+
+<script>
+import homePage from './page/homePage.vue'
+
+export default {
+  components:{
+    homePage
+  }
+ 
+}
+</script>
+
 
 <style lang="scss">
 @import './assets/css/all.scss';
