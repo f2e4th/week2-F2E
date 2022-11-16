c1b1a690428b08cfb8aa810974724254159daa03
Shang <yopshang@gmail.com>
2022-11-16 19:07:13 +0800

    新增icon及樣式

--- src/page/UploadPage.vue
+++ src/page/UploadPage.vue
@@ -1,58 +1,78 @@
 <template>
-  <div id="Upload">
-    <div class="Upload__nav">
-    </div>
+  <div id="Upload" style="height: 1440px">
+    <div class="Upload__nav"></div>
     <div class="Upload_progress">
       <ul class="progress_content">
-        <li v-for="(item, i) in progressData" :key="i" class="progress_content__item">
-          <img :src="item.img" alt="progress">
-          <div class="progress_content__item__text">{{item.text}}</div>
+        <li
+          v-for="(item, i) in progressData"
+          :key="i"
+          class="progress_content__item"
+        >
+          <img :src="item.img" alt="progress" />
+          <div class="progress_content__item__text">{{ item.text }}</div>
         </li>
       </ul>
     </div>
     <div class="Upload_content_wrapper">
-      <div class="Upload_content">
-        <div class="file"> <img src="../assets/img/icon_upload_n.png"> 選擇檔案</div>
+      <div v-if="noFile" class="Upload_content">
+        <div class="file">
+          <img src="../assets/img/icon_upload_n.png" /> 選擇檔案
+        </div>
         <div class="text">或直接拖放檔案進來</div>
         <div class="text">檔案限制格式：pdf,大小200mb以下</div>
       </div>
+      <div v-else class="Upload_content Upload_content--uploaded">
+        <div class="top_section">
+          <div class="file">
+            <img src="../assets/img/reupload.png" /> 重新上傳
+          </div>
+          <div class="text">或直接拖放檔案進來</div>
+          <div class="text">檔案限制格式：pdf,大小200mb以下</div>
+        </div>
+        <div class="bottom_section">
+          <div class="text">文件命名</div>
+          <input class="bg-white px-2 py-1" type="text" value="房地產契約test">
+        </div>
+      </div>
+
     </div>
   </div>
 </template>
 <script>
-import {ref, reactive} from 'vue';
-import finish from '../assets/img/finish.svg';
-import sign from '../assets/img/sign.svg';
-import upload from '../assets/img/upload.png';
+import { ref, reactive } from "vue";
+import finish from "../assets/img/finish.svg";
+import sign from "../assets/img/sign.svg";
+import upload from "../assets/img/upload.png";
 </script>
 <script setup>
 const progressData = reactive([
   {
-    img:upload,
-    text: '上傳簽署檔案'
-  },{
-    img:sign,
-    text: '進行簽署'
+    img: upload,
+    text: "上傳簽署檔案",
   },
   {
-    img:finish,
-    text: '簽署完成（下載）'
-  }
-])
-
-
+    img: sign,
+    text: "進行簽署",
+  },
+  {
+    img: finish,
+    text: "簽署完成（下載）",
+  },
+]);
+const noFile = ref(false); // dev for false
 </script>
 
 <style lang="scss" scoped>
-$main_color:#BE8E55;
+$main_color: #be8e55;
 #Upload {
   @apply h-screen w-full flex flex-col items-center;
   max-width: 1440px;
-  background-color: #F5F5F5;
+  background-color: #f5f5f5;
 }
 .Upload_content_wrapper {
   @apply rounded-md flex items-center justify-center my-4;
-  background-color: #EFE3D4;
+  padding: 40px;
+  background-color: #efe3d4;
   width: 600px;
   height: 600px;
   .Upload_content {
@@ -65,11 +85,12 @@ $main_color:#BE8E55;
       @apply border rounded px-2 py-1 flex flex-nowrap items-center;
       border-color: $main_color;
       margin-bottom: 8px;
-      >img {
+      > img {
         margin-right: 4px;
       }
     }
-    .file,.text{
+    .file,
+    .text {
       margin-bottom: 8px;
       font-weight: 700px;
       font-size: 16px;
@@ -83,7 +104,7 @@ $main_color:#BE8E55;
   .progress_content__item {
     @apply relative flex flex-col justify-center items-center whitespace-nowrap;
     width: 60px;
-    &:nth-last-child(1){
+    &:nth-last-child(1) {
       &::before {
         display: none;
       }
@@ -93,12 +114,12 @@ $main_color:#BE8E55;
       top: 29px;
       left: 60px;
       display: block;
-      content: '';
+      content: "";
       height: 2px;
       width: 95px;
       background-color: $main_color;
     }
-    >img {
+    > img {
       width: 60px;
       height: 60px;
     }
@@ -109,10 +130,8 @@ $main_color:#BE8E55;
     &__item {
       margin: 0 47.5px;
       &__line {
-        
       }
     }
   }
 }
-
 </style>
