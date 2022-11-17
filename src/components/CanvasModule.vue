86d05f45aa4b5dd5ebf8b556ecf2c6c1e109f657
Eva <shepherdlord1119@gmail.com>
2022-11-18 01:18:28 +0800

    新增畫筆

--- src/components/CanvasModule.vue
+++ src/components/CanvasModule.vue
@@ -1,22 +1,11 @@
 <template>
   <div class="flex flex-col items-center">
-    <div class="flex mb-6" :class="{ 'invisible': !isSignSelf }">
-      <a class="h-8 w-8 rounded-full inline-block bg-black m-2 relative" @click="setColor('#000000')">
-        <div class="h-7 w-7 rounded-full inline-block bg-black absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" :class="{ 'border-2 border-white': color === '#000000' }"></div>
-      </a>
-      <a class="h-8 w-8 rounded-full inline-block proj-bg-blue m-2 relative"  @click="setColor('#0014C7')">
-        <div class="h-7 w-7 rounded-full inline-block proj-bg-blue absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" :class="{ 'border-2 border-white': color === '#0014C7' }"></div>
-      </a>
-      <a class="h-8 w-8 rounded-full inline-block proj-bg-red m-2 relative"  @click="setColor('#CA0000')">
-        <div class="h-7 w-7 rounded-full inline-block proj-bg-red absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" :class="{ 'border-2 border-white': color === '#CA0000' }"></div>
-      </a>
-    </div>
     <div class="source px-2" v-show="isSignSelf">
       <div class="bg-white rounded-3xl max-w-xs md:max-w-none overflow-hidden">
         <vue-drawing-canvas
           ref="VueCanvasDrawing"
           v-model:image="image"
-          :width="600"
+          :width="500"
           :height="400"
           :stroke-type="strokeType"
           :line-cap="lineCap"
@@ -37,65 +26,194 @@
           :additional-images="additionalImages"
         />
       </div>
-      <div class="button-container w-full mt-6 xl:mt-10 flex text-lg">
+      <!-- <div class="button-container w-full mt-6 xl:mt-10 flex text-lg">
         <div class="w-1/2 p-2">
           <button type="button" @click.prevent="removeSavedStrokes()" class="py-3 px-3 bg-white proj-text-primary w-full rounded-lg proj-border-primary border-2">
-            清除
+            clear
           </button>
         </div>
         <div class="w-1/2 p-2">
           <button type="button" @click.prevent="getStrokes()" class="py-3 px-3 proj-bg-Gradient text-white w-full rounded-lg proj-border-primary border-2">
-            建立簽名
+            save
           </button>
         </div>
 
+      </div> -->
+    </div>
+
+    <div class="flex items-center justify-between mb-6" :class="{ invisible: !isSignSelf }" style="width:600px">
+      <div class="w-1/6 p-2">
+        <button
+          type="button"
+          @click.prevent="removeSavedStrokes()"
+          class="
+            py-3
+            px-3
+            w-full
+            rounded-lg
+            border-2
+          "
+          style="background:#FFFFFF"
+        >
+          clear
+        </button>
+      </div>
+      <div>
+        <a
+          class="h-8 w-8 rounded-full inline-block bg-black m-2 relative"
+          @click="getColor('#000000')"
+        >
+          <div
+            class="
+              h-7
+              w-7
+              rounded-full
+              inline-block
+              bg-black
+              absolute
+              left-1/2
+              top-1/2
+              -translate-x-1/2 -translate-y-1/2
+            "
+            :class="{ 'border-2 border-white': color === '#000000' }"
+          ></div>
+        </a>
+        <a
+          class="h-8 w-8 rounded-full inline-block bg-blue-700 m-2 relative"
+          @click="getColor('#0014C7')"
+        >
+          <div
+            class="
+              h-7
+              w-7
+              rounded-full
+              inline-block
+              absolute
+              left-1/2
+              top-1/2
+              -translate-x-1/2 -translate-y-1/2
+            "
+            :class="{ 'border-2 border-white': color === '#0014C7' }"
+          ></div>
+        </a>
+        <a
+          class="h-8 w-8 rounded-full inline-block bg-red-700 m-2 relative"
+          @click="getColor('#CA0000')"
+        >
+          <div
+            class="
+              h-7
+              w-7
+              rounded-full
+              inline-block
+              bg-red-700
+              absolute
+              left-1/2
+              top-1/2
+              -translate-x-1/2 -translate-y-1/2
+            "
+            :class="{ 'border-2 border-white': color === '#CA0000' }"
+          ></div>
+        </a>
+      </div>
+      <div class="w-1/6 p-2">
+        <button
+          type="button"
+          @click.prevent="getStrokes()"
+          class="
+            py-3
+            px-3
+            w-full
+            rounded-lg
+            border-2
+          "
+          style="background:#FFFFFF"
+        >
+          save
+        </button>
       </div>
     </div>
 
     <div class="output" v-show="!isSignSelf">
-
-      <div v-if="isFile" style="width: 600px; height: 400px" class="bg-white round">
+      <div
+        v-if="isFile"
+        style="width: 600px; height: 400px"
+        class="bg-white round"
+      >
         <img :src="image" style="border-radius: 26px" />
       </div>
       <div v-else style="width: 600px; height: 400px" class="bg-white round">
-        <label  for="file-upload" class="custom-file-upload w-full flex items-center justify-center">
+        <label
+          for="file-upload"
+          class="custom-file-upload w-full flex items-center justify-center"
+        >
           <span>請選擇檔案</span>
         </label>
-        <input type="file" @change="setWatermarkImage($event)" accept="image/*" id="file-upload" />
+        <input
+          type="file"
+          @change="setWatermarkImage($event)"
+          accept="image/*"
+          id="file-upload"
+        />
       </div>
 
       <div class="button-container w-full mt-10 flex text-lg">
         <div class="w-1/2 p-2">
-          <button type="button" @click.prevent="removeSavedStrokes()" class="py-3 px-3 bg-white proj-text-primary w-full rounded-lg proj-border-primary border-2">
+          <button
+            type="button"
+            @click.prevent="removeSavedStrokes()"
+            class="
+              py-3
+              px-3
+              bg-white
+              proj-text-primary
+              w-full
+              rounded-lg
+              proj-border-primary
+              border-2
+            "
+          >
             清除
           </button>
         </div>
         <div class="w-1/2 p-2">
-          <button type="button" @click.prevent="getStrokes()" class="py-3 px-3 proj-bg-Gradient text-white w-full rounded-lg proj-border-primary border-2">
+          <button
+            type="button"
+            @click.prevent="getStrokes()"
+            class="
+              py-3
+              px-3
+              proj-bg-Gradient
+              text-white
+              w-full
+              rounded-lg
+              proj-border-primary
+              border-2
+            "
+          >
             建立簽名
           </button>
         </div>
-
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import VueDrawingCanvas from "vue-drawing-canvas"
-import bus from '../srcipt/bus';
+import VueDrawingCanvas from "vue-drawing-canvas";
+import bus from "../srcipt/bus";
 export default {
   name: "CanvasModule",
   components: {
-    VueDrawingCanvas
+    VueDrawingCanvas,
   },
   props: {
     isSignSelf: {
       type: Boolean,
-      default: false
-    }
+      default: false,
+    },
   },
-  data () {
+  data() {
     return {
       initialImage: [
         {
@@ -121,32 +239,32 @@ export default {
       strokeType: "dash",
       lineCap: "square",
       lineJoin: "miter",
-      backgroundColor: "transparent",
+      backgroundColor: "transparent", 
       backgroundImage: null,
       watermark: null,
       additionalImages: [],
 
-      isFile: false
-    }
+      isFile: false,
+    };
   },
-  mounted () {
-    this.init()
+  mounted() {
+    this.init();
   },
   methods: {
-    init () {
+    init() {
       this.initialImage = [
-      {
-        type: "dash",
-        from: {
-          x: 262,
-          y: 154,
+        {
+          type: "dash",
+          from: {
+            x: 262,
+            y: 154,
+          },
+          coordinates: [],
+          color: "#000000",
+          width: 5,
+          fill: false,
         },
-        coordinates: [],
-        color: "#000000",
-        width: 5,
-        fill: false,
-      },
-    ]
+      ];
       // if ("vue-drawing-canvas" in window.localStorage) {
       //   this.initialImage = JSON.parse(
       //     window.localStorage.getItem("vue-drawing-canvas")
@@ -167,13 +285,13 @@ export default {
       //   ]
       // }
     },
-    async setImage (event) {
-      let URL = window.URL
-      this.backgroundImage = URL.createObjectURL(event.target.files[0])
-      await this.$refs.VueCanvasDrawing.redraw()
+    async setImage(event) {
+      let URL = window.URL;
+      this.backgroundImage = URL.createObjectURL(event.target.files[0]);
+      await this.$refs.VueCanvasDrawing.redraw();
     },
-    async setWatermarkImage (event) {
-      let URL = window.URL
+    async setWatermarkImage(event) {
+      let URL = window.URL;
       this.watermark = {
         type: "Image",
         source: URL.createObjectURL(event.target.files[0]),
@@ -183,42 +301,46 @@ export default {
           width: 600,
           height: 400,
         },
-      }
-      await this.$refs.VueCanvasDrawing.redraw()
-      this.isFile = true
+      };
+      await this.$refs.VueCanvasDrawing.redraw();
+      this.isFile = true;
     },
-    getCoordinate (event) {
-      let coordinates = this.$refs.VueCanvasDrawing.getCoordinates(event)
-      this.x = coordinates.x
-      this.y = coordinates.y
+    getCoordinate(event) {
+      let coordinates = this.$refs.VueCanvasDrawing.getCoordinates(event);
+      this.x = coordinates.x;
+      this.y = coordinates.y;
     },
-    getStrokes () {
-      localStorage.setItem("vue-drawing-canvas", JSON.stringify(this.$refs.VueCanvasDrawing.getAllStrokes()))
-      localStorage.setItem("vue-canvas", this.image)
+    getStrokes() {
+      localStorage.setItem(
+        "vue-drawing-canvas",
+        JSON.stringify(this.$refs.VueCanvasDrawing.getAllStrokes())
+      );
+      localStorage.setItem("vue-canvas", this.image);
 
-      let signArr
-      if (localStorage.getItem('vue-canvas-array')) {
-        signArr = JSON.parse(localStorage.getItem('vue-canvas-array'))
-        signArr.push(this.image)
+      let signArr;
+      if (localStorage.getItem("vue-canvas-array")) {
+        signArr = JSON.parse(localStorage.getItem("vue-canvas-array"));
+        signArr.push(this.image);
       } else {
-        signArr = [this.image]
+        signArr = [this.image];
       }
-      localStorage.setItem("vue-canvas-array", JSON.stringify(signArr))
+      localStorage.setItem("vue-canvas-array", JSON.stringify(signArr));
 
-      this.$emit('setStep', 1)
+      // this.$emit("setStep", 1);
+        this.$emit("sign",true)
 
-      bus.emit('reloadSign')
+      bus.emit("reloadSign");
     },
     removeSavedStrokes() {
-      window.localStorage.removeItem("vue-drawing-canvas")
+      window.localStorage.removeItem("vue-drawing-canvas");
 
-      this.$refs.VueCanvasDrawing.reset()
+      this.$refs.VueCanvasDrawing.reset();
     },
-    setColor (color) {
-      this.color = color
-    }
-  }
-}
+    getColor(color) {
+      this.color = color;
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -226,28 +348,28 @@ export default {
   margin-top: 15px;
   margin-right: 10px;
 }
-.output input[type=file] {
-    width: 0.1px;
-    height: 0.1px;
-    opacity: 0;
-    overflow: hidden;
-    position: absolute;
-    z-index: -1;
+.output input[type="file"] {
+  width: 0.1px;
+  height: 0.1px;
+  opacity: 0;
+  overflow: hidden;
+  position: absolute;
+  z-index: -1;
 }
 .round {
   border-radius: 26px;
 }
 label {
-	width: 100%;
+  width: 100%;
   height: 100%;
-	text-align: center;
+  text-align: center;
   font-size: 20px;
   font-weight: 600;
   font-family: sans-serif;
   cursor: pointer;
   /* display: inline-block; */
   overflow: hidden;
-  color: #B7B7B7;
+  color: #b7b7b7;
   background: none;
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="container_tab flex justify-center">
+    <SelectSign v-if="isSelectSign" @closeWarning="closeWarning" @selectedSign="selectedSign"  />
         <div class="flex justify-between">
+            <a class="signBtn flex flex-col items-center w-20 py-4 cursor-pointer" @click="isSelectSign = true">
             <img src="../assets/images/Tab_sign.png" @click="signModal"/>
+            </a>
             <img src="../assets/images/Tab_date.png"/>
             <img src="../assets/images/Tab_text.png"/>
         </div>
@@ -9,19 +12,55 @@
 </template>
 
 <script>
+import SelectSign from '../components/ChoiceSign.vue';
+import { onMounted, ref, reactive } from 'vue';
+var canvas = null;
 export default {
-    data(){
-        return{
-            signStatus:false,
-        }
-    },
-    methods:{
-        signModal(){
-            // this.$emitter.emit('signStatus' ,!this.signStatus)
-        }
+    components: {
+    // WarningAlert,
+    SelectSign
+  },
+  setup (props, ctx) {
+    const isSelectSign = ref(false)
+    const signUrl = ref('')
+    const closeWarning = (closeWarning) => {
+      isSelectSign.value = closeWarning
     }
 
-    
+  
+    const selectedSign = (selectedSign) => {
+      fabric.Image.fromURL(selectedSign, (image) => {
+        image.top = 100
+        image.left = 100
+        image.scaleX = 0.5
+        image.scaleY = 0.5
+        canvas.add(image)
+      })
+    }
+
+          // 加入簽名
+    const sign = document.querySelector('.signBtn')
+    //   if (localStorage.getItem('vue-canvas')) {
+    //     // sign.src = localStorage.getItem('vue-canvas')
+    //     signUrl.value = localStorage.getItem('vue-canvas')
+    //   }
+    //   sign.addEventListener('click', () => {
+    //     if (!signUrl.value) return
+    //     fabric.Image.fromURL(signUrl.value, (image) => {
+    //       image.top = 100
+    //       image.left = 100
+    //       image.scaleX = 0.5
+    //       image.scaleY = 0.5
+    //       canvas.add(image)
+    //     })
+    //   })
+    return {
+        isSelectSign,
+        closeWarning,
+        selectedSign,
+        signUrl,
+    }
+  } 
 }
 </script>
 
